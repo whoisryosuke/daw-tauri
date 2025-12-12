1c21011f4364b185cae480ade8176d974f7695d7
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-12-12 14:53:01 -0800

    :construction: ringbuf process

--- src-tauri/src/audio_engine.rs
+++ src-tauri/src/audio_engine.rs
@@ -1,12 +1,9 @@
-use std::sync::Arc;
+use std::sync::{Arc, atomic::{AtomicBool, AtomicUsize, Ordering::Relaxed}};
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use ringbuf::HeapRb;
+use ringbuf::{HeapRb, SharedRb, storage::Heap, traits::{Consumer, Split}, wrap::caching::Caching};
 
-struct PlaybackBuffer {
-    data: Vec<f32>,
-    position: usize,
-}
+const SAMPLE_BUFFER_SIZE: usize = 1024;
 
 pub struct AudioEngine {
     /**
@@ -16,8 +13,12 @@ pub struct AudioEngine {
     
     // device: &cpal::Device,
     config: cpal::StreamConfig,
-    playback_buffer: ArcSwap<PlaybackBuffer>,
-    heap: HeapRb<f32>,
+    
+    // heap: HeapRb<f32>,
+    producer: Caching<Arc<SharedRb<Heap<f32>>>, true, false>,
+
+    position: Arc<AtomicUsize>,
+    finished: AtomicBool,
 }
 
 impl AudioEngine {
@@ -29,23 +30,27 @@ impl AudioEngine {
             .expect("no output device available");
 
         let config = device.default_output_config().expect("Couldn't load config");
-         
-        let initial_playback_buffer = Arc::new(
-            PlaybackBuffer{
-                data: vec![0.0; 1],
-                position: 0,
-            }
-        );
 
-        let playback_buffer = ArcSwap::new(initial_playback_buffer.clone());
-        let playback_buffer_copy = playback_buffer.clone();
+        // Heap
+        let heap = HeapRb::<f32>::new(SAMPLE_BUFFER_SIZE);
+        let (mut producer, mut consumer) = heap.split();
+         
+        // Create audio state
+        // These need to be atomic so they can be updated
+        let position = Arc::new(AtomicUsize::new(0usize));
+        let finished = AtomicBool::new(false);
+        // let waveform_data: Arc<Mutex<VecDeque<f32>>> = Arc::new(Mutex::new(VecDeque::with_capacity(WAVEFORM_SAMPLE_NUM)));
+
+        // let playback_buffer = ArcSwap::new(initial_playback_buffer.clone());
+        // let playback_buffer_copy = playback_buffer.clone();
 
         let channels = config.channels() as usize;
 
         
+        let position_clone = Arc::clone(&position);
         let stream = match config.sample_format() {
-            cpal::SampleFormat::F32 => device.build_output_stream(&config.into(), move |output: &mut [f32], _| {
-                Self::render(output, channels, &playback_buffer_copy);
+            cpal::SampleFormat::F32 => device.build_output_stream(&config.clone().into(), move |output: &mut [f32], _| {
+                Self::render(output, channels, &mut consumer, &position_clone);
             },
         |err| eprintln!("couldn't build audio stream: {err}"), None).expect("couldn't build audio stream"),
             cpal::SampleFormat::I16 =>  todo!(),
@@ -66,28 +71,34 @@ impl AudioEngine {
         Self {
             config: config.into(),
             stream,
-            playback_buffer
+            position,
+            producer,
+            // heap,
+            finished,
         }
     }
 
-    fn render(output: &mut [f32], channels: usize, buffer: &ArcSwap<PlaybackBuffer>) {
-        let mut sample = buffer.load_full();
+    fn render(output: &mut [f32], channels: usize, consumer: &mut Caching<Arc<SharedRb<Heap<f32>>>, false, true>, position: &Arc<AtomicUsize>,) {
+        // Read from ring buffer
+        let mut samples = vec![0.0; SAMPLE_BUFFER_SIZE];
+        let read = consumer.occupied_slices(&mut samples);
+        
+        let sample_index = position.load(Relaxed);
 
+        // Loop over the output and override with new audio
         for frame in output.chunks_mut(channels) {
             
             // Replace output channel with sample data
             for ch in 0..channels {
-                if sample.position < sample.data.len() {
+                if sample_index < samples.len() {
                     // Override output with our sample
-                    frame[ch] = sample.data[sample.position];
+                    frame[ch] = samples[sample_index];
                     // Increment our sample array index counter
-                    sample.position += 1;
+                    position.store(sample_index + 1, Relaxed);
                 } else {
                     frame[ch] = 0.0;
                 }
             }
         }
-
-        ArcSwap::store(buffer, sample);
     }
 }
