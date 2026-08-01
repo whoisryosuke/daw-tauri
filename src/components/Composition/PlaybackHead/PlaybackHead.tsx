37d63b603f39261bd9e2fe92602f76e89c1cf036
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2026-08-01 00:40:14 -0700

    :hammer: use motion + RAF for smoother animation avoiding VDOM

--- src/components/Composition/PlaybackHead/PlaybackHead.tsx
+++ src/components/Composition/PlaybackHead/PlaybackHead.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from "react";
+import React, { CSSProperties, useEffect, useRef } from "react";
 import { sva } from "../../../../styled-system/css";
 import { usePlaybackTime } from "../../../hooks/usePlaybackTime";
 import { useAtomValue } from "jotai/react";
@@ -6,6 +6,7 @@ import { compositionAtom } from "../../../store/composition";
 import mapRange from "../../../utils/map";
 import { VscTriangleDown } from "react-icons/vsc";
 import { ColorToken } from "../../../../styled-system/tokens";
+import { motion, useMotionValue, useTransform } from "motion/react";
 
 const lineColor: ColorToken = "gray.6";
 
@@ -16,7 +17,6 @@ const styles = sva({
       position: "absolute",
       top: 0,
       left: 0,
-      transform: "translateX(var(--x))",
 
       height: "100%",
     },
@@ -45,21 +45,43 @@ type Props = {
 };
 
 const PlaybackHead = ({ containerWidth }: Props) => {
-  const { time } = usePlaybackTime();
+  const animationRef = useRef<ReturnType<typeof requestAnimationFrame>>(null);
+  const { getTime } = usePlaybackTime();
   const { range } = useAtomValue(compositionAtom);
   const classes = styles();
+  const x = useMotionValue(0);
+  // TODO: Create global state of play/pause
+  const isPlaying = true;
 
-  const x = mapRange(time, range[0], range[1], 0, containerWidth);
+  // Map the time (in seconds) to a horizontal position on timeline
+  const translateX = useTransform(x, [range[0], range[1]], [0, containerWidth]);
+
+  const animate = () => {
+    const time = getTime();
+
+    x.set(time);
+
+    animationRef.current = requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      animationRef.current = requestAnimationFrame(animate);
+    } else {
+      if (animationRef.current) cancelAnimationFrame(animationRef.current);
+    }
+    return () => {
+      if (animationRef.current) cancelAnimationFrame(animationRef.current);
+    };
+  }, [isPlaying]);
+
   return (
-    <div
-      className={classes.container}
-      style={{ "--x": `${x}px` } as CSSProperties}
-    >
+    <motion.div className={classes.container} style={{ x: translateX }}>
       <div className={classes.line} />
       <div className={classes.marker}>
         <VscTriangleDown />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
