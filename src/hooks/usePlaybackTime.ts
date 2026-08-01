34ba3358ac32b55271522a2569a55417ecee6c73
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2026-08-01 00:39:52 -0700

    :hammer: convert to ref for now - but this system will change

--- src/hooks/usePlaybackTime.ts
+++ src/hooks/usePlaybackTime.ts
@@ -3,15 +3,17 @@ import { listen, UnlistenFn } from "@tauri-apps/api/event";
 import { useEffect, useRef, useState } from "react";
 
 export function usePlaybackTime() {
-  const [sampleRate, setSampleRate] = useState(0);
-  const [timeInFrames, setTimeInFrames] = useState(0);
+  const sampleRate = useRef(0);
+  const timeInFrames = useRef(0);
+  // const [sampleRate, setSampleRate] = useState(0);
+  // const [timeInFrames, setTimeInFrames] = useState(0);
   const listenerRef = useRef<UnlistenFn>(null);
 
   useEffect(() => {
     const getSampleRate = async () => {
       const newSampleRate = (await invoke("get_sample_rate")) as number;
       console.log("sample rate", newSampleRate);
-      setSampleRate(newSampleRate);
+      sampleRate.current = newSampleRate;
     };
 
     getSampleRate();
@@ -20,7 +22,7 @@ export function usePlaybackTime() {
   useEffect(() => {
     const attachEvents = async () => {
       listenerRef.current = await listen("playback_time", (event) => {
-        setTimeInFrames(event.payload as number);
+        timeInFrames.current = event.payload as number;
       });
     };
 
@@ -31,7 +33,7 @@ export function usePlaybackTime() {
     };
   });
 
-  const time = timeInFrames / sampleRate;
+  const getTime = () => timeInFrames.current / sampleRate.current;
 
-  return { sampleRate, timeInFrames, time };
+  return { sampleRate, timeInFrames, getTime };
 }
