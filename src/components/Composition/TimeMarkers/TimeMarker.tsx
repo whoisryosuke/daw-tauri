9b590369576fc9b4a0790a660c0c6cf8459accfa
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2026-08-02 00:15:17 -0700

    :lipstick: number and arrow

--- src/components/Composition/TimeMarkers/TimeMarker.tsx
+++ src/components/Composition/TimeMarkers/TimeMarker.tsx
@@ -1,17 +1,32 @@
 import React, { CSSProperties, PropsWithChildren } from "react";
 import { css } from "../../../../styled-system/css";
 import Text from "../../ui/Typography/Text";
+import { VscTriangleDown } from "react-icons/vsc";
 
 const containerStyle = css({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  transform: "translateX(-50%)",
+  color: "gray.9",
+  gap: 0,
+});
+const textStyle = css({
   fontSize: 1,
-  color: "gray.10",
   fontFamily: "mono",
+  pointerEvents: "none",
+  userSelect: "none",
 });
 
 type Props = {};
 
 const TimeMarker = ({ children }: PropsWithChildren<Props>) => {
-  return <Text className={containerStyle}>{children}</Text>;
+  return (
+    <div className={containerStyle}>
+      <Text className={textStyle}>{children}</Text>
+      <VscTriangleDown size={8} />
+    </div>
+  );
 };
 
 export default TimeMarker;
