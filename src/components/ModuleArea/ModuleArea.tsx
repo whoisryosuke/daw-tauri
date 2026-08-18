a344745daac46ad79eeb3557ad2d54fbcff73d1e
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2026-08-18 15:19:44 -0700

    :fire: remove debug toast

--- src/components/ModuleArea/ModuleArea.tsx
+++ src/components/ModuleArea/ModuleArea.tsx
@@ -4,14 +4,12 @@ import DebugSynthControls from "../debug/DebugSynthControls";
 import { Box, Stack } from "../../../styled-system/jsx";
 import EffectModules from "./EffectModules/EffectModules";
 import MidiModule from "./TrackModules/MidiModule";
-import ToastButton from "../ui/Toast/ToastButton";
 
 type Props = {};
 
 const ModuleArea = (props: Props) => {
   return (
     <Stack flexDir="row" gap="0" height="100%" p={2}>
-      <ToastButton />
       <Waveform />
       <MidiModule />
       <EffectModules />
